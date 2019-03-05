f04db48692f8e63462b02e9a4341e3e27f10d79c
zismailov <zaur_ismailov@epam.com>
2019-03-05 12:58:05 +0300

    Add new packages.

---
+++ packages/core/src/index.ts
@@ -0,0 +1,21 @@
+export const coreFn = () => {
+  console.log('coreFn message');
+  console.log('new message');
+  console.log('new message1');
+  console.log('new message2');
+  console.log('new message3');
+  console.log('new message4');
+  console.log('new message5');
+  console.log('new message6');
+  console.log('new message7');
+  console.log('new message8');
+  console.log('new message9');
+  console.log('new message10');
+  console.log('new message11');
+  console.log('new message12');
+  console.log('new message13');
+  console.log('new message14');
+  console.log('new message15');
+  console.log('new message16');
+  console.log('new message17');
+};
---
+++ packages/logger/src/index.ts
@@ -0,0 +1,17 @@
+export const loggerFn = () => {
+  console.log('loggerFn extra message');
+  console.log('logger message1');
+  console.log('logger message2');
+  console.log('logger message3');
+  console.log('logger message4');
+  console.log('logger message5');
+  console.log('logger message6');
+  console.log('logger message7');
+  console.log('logger message8');
+  console.log('logger message9');
+  console.log('logger message10');
+  console.log('logger message11');
+  console.log('logger message12');
+  console.log('logger message13');
+  console.log('logger message14');
+};
